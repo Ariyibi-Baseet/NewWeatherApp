2471d56c24e524981ac120c642ac71d9c40c4e9e
Ariyibi-Baseet <ariyibi.baseet@gmail.com>
2024-04-03 12:34:25 +0100

    hardcoded the Base url and the key

--- src/stores/weather.js
+++ src/stores/weather.js
@@ -1,14 +1,16 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
-import { api, apiKey } from '@/api/utils'
+// import { api, apiKey } from '@/api/utils'
 import { ref } from 'vue'
 
 export const useWeatherStore = defineStore('weather', () => {
   const errMessage = ref('')
   const getCurrentWeather = async (city) => {
     try {
-      const apiUrl = `${api}/current.json?key=${apiKey}&q=${city}`
-      const response = await axios.get(apiUrl)
+      // const apiUrl = `${api}/current.json?key=${apiKey}&q=${city}`
+      const apiURL =
+        'https://api.weatherapi.com/v1/current.json?key=267e2861260b4cd7ba4172707231105&q=' + city
+      const response = await axios.get(apiURL)
       return response
     } catch (error) {
       // console.log(error.response.data.error)
