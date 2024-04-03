8be4db7ad5ae5b682d1fffef9e557e868de950ba
Ariyibi-Baseet <ariyibi.baseet@gmail.com>
2024-04-03 12:42:22 +0100

    change back to dynamic

--- src/stores/weather.js
+++ src/stores/weather.js
@@ -5,12 +5,16 @@ import { ref } from 'vue'
 
 export const useWeatherStore = defineStore('weather', () => {
   const errMessage = ref('')
+
+  const api = import.meta.env.VITE_BASE_URL
+  const apiKey = import.meta.env.VITE_API_KEY
+
   const getCurrentWeather = async (city) => {
     try {
-      // const apiUrl = `${api}/current.json?key=${apiKey}&q=${city}`
-      const apiURL =
-        'https://api.weatherapi.com/v1/current.json?key=267e2861260b4cd7ba4172707231105&q=' + city
-      const response = await axios.get(apiURL)
+      const apiUrl = `${api}/current.json?key=${apiKey}&q=${city}`
+      // const apiURL =
+      //   'https://api.weatherapi.com/v1/current.json?key=267e2861260b4cd7ba4172707231105&q=' + city
+      const response = await axios.get(apiUrl)
       return response
     } catch (error) {
       // console.log(error.response.data.error)
